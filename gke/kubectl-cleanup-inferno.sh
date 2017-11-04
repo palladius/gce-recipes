b49ebb00093e9a92914c350e4a64b6fb1cb8db7e
Riccardo Carlesso <palladiusbonton+derek.zrh.corp.google.com@gmail.com>
2017-11-04 15:17:50 +0100

    more ideas

---
+++ gke/kubectl-cleanup-inferno.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# kubectl-cleanup-inferno.sh
+
+# cleanup redis PHP guestbook
+for APP in redis guestbook ; do
+	kubectl delete deployment -l app=$APP
+	kubectl delete service -l app=$APP
+end
