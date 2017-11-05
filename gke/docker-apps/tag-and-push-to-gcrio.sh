4da97f7f4eca5b7051d5d2983badfc92d86b4e3b
Riccardo Carlesso @Rubino <palladiusbonton+rubino@gmail.com>
2017-11-05 14:57:44 +0000

    v1.4

--- gke/docker-apps/tag-and-push-to-gcrio.sh
+++ gke/docker-apps/tag-and-push-to-gcrio.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+VERSION=1.1
+
 IMAGE_NAME=${1:-sobenme}
 IMAGE_VERSION=${2:-0.0.1alpha}
 
 GCLOUD="sudo /usr/local/google-cloud-sdk/bin/gcloud"
 
-echo "Forcing latest to be latest and not forcing the VERSION so you only do it once ideally (immutable)"
+echo "1. Forcing latest to be latest and not forcing the VERSION so you only do it once ideally (immutable)"
+sudo docker tag $IMAGE_NAME gcr.io/ric-cccwiki/$IMAGE_NAME:v$IMAGE_VERSION
 sudo docker tag -f $IMAGE_NAME gcr.io/ric-cccwiki/$IMAGE_NAME
 sudo docker tag -f $IMAGE_NAME gcr.io/ric-cccwiki/$IMAGE_NAME:latest
-sudo docker tag $IMAGE_NAME gcr.io/ric-cccwiki/$IMAGE_NAME:v$IMAGE_VERSION
 
+echo "2. Pushing now to gcr.io/ric-cccwiki:"
 # this doesnt work as requires root :/
-#gcloud docker -- push gcr.io/ric-cccwiki/$IMAGE_NAME
-$GCLOUD docker -- push -f gcr.io/ric-cccwiki/$IMAGE_NAME
 $GCLOUD docker -- push gcr.io/ric-cccwiki/$IMAGE_NAME:v$IMAGE_VERSION
+$GCLOUD docker -- push gcr.io/ric-cccwiki/$IMAGE_NAME
 $GCLOUD docker -- push gcr.io/ric-cccwiki/$IMAGE_NAME:latest
