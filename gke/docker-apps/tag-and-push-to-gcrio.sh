4f018654babeb8474af6794af4de85e53ffdf566
Riccardo Carlesso <palladiusbonton+derek.zrh.corp.google.com@gmail.com>
2017-11-24 11:09:31 +0100

    funge

--- gke/docker-apps/tag-and-push-to-gcrio.sh
+++ gke/docker-apps/tag-and-push-to-gcrio.sh
@@ -5,12 +5,13 @@ VERSION=1.1
 IMAGE_NAME=${1:-sobenme}
 IMAGE_VERSION=${2:-0.0.1alpha}
 
-GCLOUD="sudo /usr/local/google-cloud-sdk/bin/gcloud"
+#GCLOUD="sudo /usr/local/google-cloud-sdk/bin/gcloud"
+GCLOUD="gcloud"
 
 echo "1. Forcing latest to be latest and not forcing the VERSION so you only do it once ideally (immutable)"
-sudo docker tag $IMAGE_NAME gcr.io/ric-cccwiki/$IMAGE_NAME:v$IMAGE_VERSION
-sudo docker tag -f $IMAGE_NAME gcr.io/ric-cccwiki/$IMAGE_NAME
-sudo docker tag -f $IMAGE_NAME gcr.io/ric-cccwiki/$IMAGE_NAME:latest
+docker tag $IMAGE_NAME gcr.io/ric-cccwiki/$IMAGE_NAME:v$IMAGE_VERSION
+docker tag -f $IMAGE_NAME gcr.io/ric-cccwiki/$IMAGE_NAME
+docker tag -f $IMAGE_NAME gcr.io/ric-cccwiki/$IMAGE_NAME:latest
 
 echo "2. Pushing now to gcr.io/ric-cccwiki:"
 # this doesnt work as requires root :/
