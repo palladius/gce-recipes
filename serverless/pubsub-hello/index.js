5fa5a80155d31c1b22e0ed100fcdd168bb75ce89
Riccardo Carlesso <palladiusbonton@gmail.com>
2017-12-06 17:29:11 +0100

    better names

--- serverless/pubsub-hello/index.js
+++ serverless/pubsub-hello/index.js
@@ -4,7 +4,7 @@
  * @param {object} event The Cloud Functions event.
  * @param {function} callback The callback function.
  */
-exports.helloWorld = function helloWorld (event, callback) {
+exports.helloWorldPS = function helloWorldPS (event, callback) {
   console.log(`My Cloud Function: ${event.data.message}`);
   callback();
 };
