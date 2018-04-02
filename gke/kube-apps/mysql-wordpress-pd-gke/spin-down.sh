d3939985b09856b46101bff16633ded7b1899946
Riccardo Carlesso <palladiusbonton+derek.zrh.corp.google.com@gmail.com>
2018-04-02 09:04:45 +0200

    adding common.

--- gke/kube-apps/mysql-wordpress-pd-gke/spin-down.sh
+++ gke/kube-apps/mysql-wordpress-pd-gke/spin-down.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. common.sh
+
 # Docs: https://kubernetes.io/docs/tutorials/stateful-application/mysql-wordpress-persistent-volume/
 
 kubectl delete secret mysql-pass
@@ -8,4 +10,4 @@ kubectl delete deployment -l app=wordpress
 kubectl delete service -l app=wordpress
 #Run the following commands to delete the PersistentVolumeClaims and the PersistentVolumes:
 kubectl delete pvc -l app=wordpress
-kubectl delete pv local-pv-1 local-pv-2
+#kubectl delete pv local-pv-1 local-pv-2
--- gke/kube-apps/mysql-wordpress-pd-gke/spin-up.sh
+++ gke/kube-apps/mysql-wordpress-pd-gke/spin-up.sh
@@ -3,8 +3,7 @@
 # fatto SOLO per k8s su GKE che e' diverso...
 # https://cloud.google.com/container-engine/docs/tutorials/persistent-disk
 
-ZONE=europe-west1-b
-PROJECT_ID=ric-cccwiki
+. common.sh
 
 if [ "aaa$MYSQL_ROOT_PASSWORD" = "aaa" ]; then
 	echo MYSQL_ROOT_PASSWORD env not found. Please set export MYSQL_ROOT_PASSWORD=whateverPassYouWant
@@ -14,11 +13,10 @@ fi
 set -x
 
 # GCE prerequisites
-gcloud --project "$PROJECT_ID" compute disks create --zone "$ZONE" --size 200GB mysql-disk
-gcloud --project "$PROJECT_ID" compute disks create --zone "$ZONE" --size 200GB wordpress-disk
-gcloud --project "$PROJECT_ID" compute disks create --zone "europe-west1-d" --size 200GB mysql-disk
-gcloud --project "$PROJECT_ID" compute disks create --zone "europe-west1-d" --size 200GB wordpress-disk
-
+gcloud --project "$PROJECT_ID" compute disks create --zone "$ZONE" --size 200GB $MYSQLDISKNAME
+gcloud --project "$PROJECT_ID" compute disks create --zone "$ZONE" --size 200GB $WORDPRESS_DISK_NAME
+#gcloud --project "$PROJECT_ID" compute disks create --zone "europe-west1-d" --size 200GB $MYSQLDISKNAME
+#gcloud --project "$PROJECT_ID" compute disks create --zone "europe-west1-d" --size 200GB $WORDPRESS_DISK_NAME
 
 kubectl create secret generic mysql-pass --from-literal=password=$MYSQL_ROOT_PASSWORD
 
@@ -28,22 +26,11 @@ kubectl create -f wordpress.yaml
 kubectl create -f wordpress-service.yaml
 
 #kubectl create -f local-volumes.yaml
-
-#kubectl get pv
-
-#kubectl create -f local-volumes.yaml
-
-#kubectl get pv
-
-#kubectl create secret generic mysql-pass --from-literal=password=YOUR_PASSWORD
-
-#kubectl get secrets
-
 #kubectl create -f mysql-deployment.yaml
-
-#kubectl get pods
-
 #kubectl create -f wordpress-deployment.yaml
 
+#kubectl get pv
+#kubectl get secrets
+#kubectl get pods
 #kubectl get services wordpress
 
